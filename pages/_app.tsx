f67e4a71763786eba9dff49c5d6f5b3fcfee2352
Phuc Tran <tqp1610@gmail.com>
2023-03-20 00:03:06 +0700

    update app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,42 @@
+import { useEffect } from 'react';
+import Layout from '@/components/Layout'
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
-import Layout from '../components/Layout';
+import Login from './login';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, db } from '@/config/firebase';
+import Loading from '../components/Loading';
+import { doc, serverTimestamp, setDoc } from 'firebase/firestore';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [loggedInUser, loading, _error] = useAuthState(auth);
+
+  useEffect(() => {
+    const setUserInDb = async () => {
+      try {
+        await setDoc(
+          doc(db, 'users', loggedInUser?.uid as string), {
+            email: loggedInUser?.email,
+            lastSeen: serverTimestamp(),
+            photoURL: loggedInUser?.photoURL
+        },
+          {merge: true} // just  update what is changed
+        );
+      } catch (error) {
+        console.log('ERROR SETTING USER INFO IN DB', error);
+      }
+    }
+    if (loggedInUser) {
+      setUserInDb();
+    }
+  }, [loggedInUser])
+  
+
+  if(loading) return <Loading/>
+  if (!loggedInUser) return <Login />
   return (
-    <Layout>
+    // <Layout>
     <Component {...pageProps} />
-  </Layout>)
+  // </Layout >
+  )
 }
