06eccb2d2c157fadded1b05593c2ea949f5e56ff
Phuc Tran <tqp1610@gmail.com>
2023-03-19 14:44:38 +0700

    setup

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { login, register } from "@/service/AuthService";
+import { useRouter } from "next/router";
 
+export default function Login() {
+    const router = useRouter();
+    const [view, setView] = useState<"log" | "reg">("log");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-export default function login() {
+    const togle = async () => {
+        setView(view === "log" ? "reg" : "log");
+    }
+
+    const star = async () => {
+        if (!email == !password) return;
+        const action = view === "log" ? login : register;
+        try {
+            await action(email, password);
+            router.replace("/");
+        } catch (err){
+            console.log(err);
+        }
+    }
+
     return <div>
-        
+
     </div>;
 }
---
+++ services/AuthService.ts
@@ -0,0 +1,3 @@
+export const register = (email: string, password: string) => {
+    
+}
