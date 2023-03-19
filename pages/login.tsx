3e6b177de02c59dcc7bfe39452f64fc04d87b015
Phuc Tran <tqp1610@gmail.com>
2023-03-20 00:03:14 +0700

    update login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,29 +1,49 @@
-import React, { useState } from "react";
-import { login, register } from "@/service/AuthService";
-import { useRouter } from "next/router";
+import React from 'react';
+import Head from 'next/head';
+import styled from 'styled-components';
+import { Button } from '@mui/material';
+import Image from 'next/image';
+import WhatsAppLogo from '../assets/logo.jpg';
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { auth } from '@/config/firebase';
+
+const StyledContainer = styled.div`
+    height: 100vh;
+    display: grid;
+    place-items:center;
+    background-color: whitesmoke;
+`
+const StyledLoginContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 100px;
+    background-color: white;
+    border-radius: 5px;
+    box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+`
+const StyleImageWrapper = styled.div`
+    margin-bottom: 50px;
+`
 
 export default function Login() {
-    const router = useRouter();
-    const [view, setView] = useState<"log" | "reg">("log");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [signInWithGoogle, _user, _loading, _error] = useSignInWithGoogle(auth);
 
-    const togle = async () => {
-        setView(view === "log" ? "reg" : "log");
+    const signIn = () => {
+        signInWithGoogle();
     }
 
-    const star = async () => {
-        if (!email == !password) return;
-        const action = view === "log" ? login : register;
-        try {
-            await action(email, password);
-            router.replace("/");
-        } catch (err){
-            console.log(err);
-        }
-    }
-
-    return <div>
-
-    </div>;
+    return (
+        <StyledContainer>
+            <Head>
+                <title>Login</title>
+            </Head>
+            <StyledLoginContainer>
+                <StyleImageWrapper>
+                    <Image src={WhatsAppLogo} alt="WhatsApp Logo" height={200} width={200} />
+                </StyleImageWrapper>
+                <Button variant='outlined' onClick={signIn}>SIGN IN WITH GOOGLE</Button>
+            </StyledLoginContainer>
+        </StyledContainer>
+    )
 }
