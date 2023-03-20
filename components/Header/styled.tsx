fa8d59b80f9d2beee4b12f1175880dc64ed98af4
Phuc Tran <tqp1610@gmail.com>
2023-03-21 00:21:50 +0700

    update header

---
+++ components/Header/styled.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.div`
+    display: flex;
+    flex: 1;
+    height: 80px;
+    width: 100%;
+    padding: 0 16px;
+    justify-content: space-between;
+    border: 1px solid #f0f0f0;
+`
+
+export const StyledHeaderWrapper = styled.div`
+    
+`
+
+export const StyledHeaderNav = styled.div`
+    display: flex;
+    list-style-type: none;
+`
+
+export const StyledHeaderNavItem = styled.li`
+    padding: 32px 24px;
+    font-weight: 700;
+    line-height: 1.6;
+    position: relative;
+    :hover {
+        cursor: pointer;
+    }
+`
+
+export const StyledHeaderAfterNav = styled.div`
+    
+`
+
+export const StyledHeaderRightWrapper = styled.div`
+    
+`
