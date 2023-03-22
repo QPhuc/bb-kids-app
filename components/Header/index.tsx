7e7399a563b87c08d96a49930888acf10cc76671
Phuc Tran <tqp1610@gmail.com>
2023-03-22 21:22:13 +0700

    header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,8 +1,12 @@
 import Image from 'next/image'
 import React from 'react'
-import { StyledHeader, StyledHeaderWrapper, StyledHeaderNav, StyledHeaderRightWrapper, StyledHeaderAfterNav, StyledHeaderNavItem } from './styled';
+import { StyledHeader, StyledHeaderWrapper, StyledHeaderNav, StyledHeaderRightWrapper, StyledHeaderAfterNav, StyledHeaderNavItem, StyledIcon, StyledWrapperIcon, StyledBasketIcon, StyledBasketCount } from './styled';
 import Logo from '@/assets/logo.jpg';
 import styles from './styles.module.scss';
+import SearchSharpIcon from '@mui/icons-material/SearchSharp';
+import PersonOutlineSharpIcon from '@mui/icons-material/PersonOutlineSharp';
+import KeyboardArrowDownSharpIcon from '@mui/icons-material/KeyboardArrowDownSharp';
+import WorkOutlineSharpIcon from '@mui/icons-material/WorkOutlineSharp';
 
 export default function Header() {
   return (
@@ -33,11 +37,20 @@ export default function Header() {
           </StyledHeaderNavItem>
         </StyledHeaderNav>
         <StyledHeaderAfterNav>
-          
+          <StyledIcon>
+            <SearchSharpIcon fontSize="inherit" />
+            <StyledWrapperIcon>
+              <PersonOutlineSharpIcon fontSize="inherit" />
+              <KeyboardArrowDownSharpIcon fontSize="inherit" />
+            </StyledWrapperIcon>
+          </StyledIcon>
         </StyledHeaderAfterNav>
       </StyledHeaderWrapper>
       <StyledHeaderRightWrapper>
-
+        <StyledBasketCount>0</StyledBasketCount>
+        <StyledBasketIcon>
+          <WorkOutlineSharpIcon fontSize="inherit" />
+        </StyledBasketIcon>
       </StyledHeaderRightWrapper>
     </StyledHeader>
   )
