bd06bfab2000d6a9a7f7c1f64bc42c4b27f21e64
Phuc Tran <tqp1610@gmail.com>
2023-03-26 00:07:31 +0700

    update footer

--- components/Footer/styled.tsx
+++ components/Footer/styled.tsx
@@ -1,5 +1,50 @@
 import styled from "styled-components";
 
-export const Footer = styled.div`
+export const StyledFooter = styled.footer`
+    border-top: 1px solid var(--grey-white-color);
+    margin-bottom: px;
+`
+
+export const StyledFooterPromo = styled.div`
+    padding: 0 16px;
+    height: 40px;
+`
+
+export const StyledFooterPromoItem = styled.div`
     
+`
+
+export const StyledFooterInfo = styled.div`
+    background: repeating-linear-gradient(90deg,var(--primary-color), var(--primary-color-2), 5%,var(--light-color) 0,var(--light-color) 10%);
+    min-height: 10rem;
+    padding: 16px 0 0;
+    border-top: 1px solid var(--grey-white-color);
+    border-bottom: 1px solid var(--grey-white-color);
+`
+
+export const StyledFooterMenuTitle = styled.span`
+    text-transform: uppercase;
+    text-align: center;
+    display: block;
+    letter-spacing: 0.025em;
+    cursor: pointer;
+    margin-bottom: 16px;
+`
+
+export const StyledFooterMenu = styled.div`
+    margin: 0 120px;
+    padding: 24px 0;
+`
+
+export const StyledFooterMenuList = styled.ul`
+    padding: 0;
+`
+
+export const StyledFooterMenuItem = styled.li`
+    text-align: center;
+    margin: 8px 0;
+    line-height: 1.6;
+    &:hover {
+        text-decoration: underline;
+    }
 `
