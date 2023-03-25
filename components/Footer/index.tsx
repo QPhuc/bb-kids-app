c3d952aedc08084302bfbbb056b30090e1eb7a8b
Phuc Tran <tqp1610@gmail.com>
2023-03-26 00:07:44 +0700

    footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,7 +1,65 @@
 import React from 'react'
+import { StyledFooter, StyledFooterInfo, StyledFooterPromo, StyledFooterPromoItem, StyledFooterMenu, StyledFooterMenuList, StyledFooterMenuItem, StyledFooterMenuTitle } from './styled';
 
 export default function Footer() {
     return (
-        <div>Footer</div>
+        <StyledFooter>
+            <StyledFooterPromo>
+                <div className='row'>
+                    <div className='col-3'>
+                        <StyledFooterPromoItem>
+                        
+                        </StyledFooterPromoItem>
+                    </div>
+                </div>
+            </StyledFooterPromo>
+            <StyledFooterInfo>
+
+            </StyledFooterInfo>
+            <StyledFooterMenu>
+                <div className='row'>
+                    <div className='col-3'>
+                        <StyledFooterMenuTitle>Customer Service</StyledFooterMenuTitle>
+                        <StyledFooterMenuList>
+                            <StyledFooterMenuItem><a href='#'>Need Help?</a></StyledFooterMenuItem>
+                            <StyledFooterMenuItem><a href='#'>Accessibity Statement</a></StyledFooterMenuItem>
+                            <StyledFooterMenuItem><a href='#'>My Account</a></StyledFooterMenuItem>
+                            <StyledFooterMenuItem><a href='#'>Track Your Order</a></StyledFooterMenuItem>
+                            <StyledFooterMenuItem><a href='#'>FAQs</a></StyledFooterMenuItem>
+                        </StyledFooterMenuList>
+                    </div>
+                    <div className='col-3'>
+                        <StyledFooterMenuTitle>Customer Service</StyledFooterMenuTitle>
+                        <StyledFooterMenuList>
+                            <StyledFooterMenuItem><a href='#'>Need Help?</a></StyledFooterMenuItem>
+                            <StyledFooterMenuItem><a href='#'>Accessibity Statement</a></StyledFooterMenuItem>
+                            <StyledFooterMenuItem><a href='#'>My Account</a></StyledFooterMenuItem>
+                            <StyledFooterMenuItem><a href='#'>Track Your Order</a></StyledFooterMenuItem>
+                            <StyledFooterMenuItem><a href='#'>FAQs</a></StyledFooterMenuItem>
+                        </StyledFooterMenuList>
+                    </div>
+                    <div className='col-3'>
+                        <StyledFooterMenuTitle>Customer Service</StyledFooterMenuTitle>
+                        <StyledFooterMenuList>
+                            <StyledFooterMenuItem><a href='#'>Need Help?</a></StyledFooterMenuItem>
+                            <StyledFooterMenuItem><a href='#'>Accessibity Statement</a></StyledFooterMenuItem>
+                            <StyledFooterMenuItem><a href='#'>My Account</a></StyledFooterMenuItem>
+                            <StyledFooterMenuItem><a href='#'>Track Your Order</a></StyledFooterMenuItem>
+                            <StyledFooterMenuItem><a href='#'>FAQs</a></StyledFooterMenuItem>
+                        </StyledFooterMenuList>
+                    </div>
+                    <div className='col-3'>
+                        <StyledFooterMenuTitle>Customer Service</StyledFooterMenuTitle>
+                        <StyledFooterMenuList>
+                            <StyledFooterMenuItem><a href='#'>Need Help?</a></StyledFooterMenuItem>
+                            <StyledFooterMenuItem><a href='#'>Accessibity Statement</a></StyledFooterMenuItem>
+                            <StyledFooterMenuItem><a href='#'>My Account</a></StyledFooterMenuItem>
+                            <StyledFooterMenuItem><a href='#'>Track Your Order</a></StyledFooterMenuItem>
+                            <StyledFooterMenuItem><a href='#'>FAQs</a></StyledFooterMenuItem>
+                        </StyledFooterMenuList>
+                    </div>
+                </div>
+            </StyledFooterMenu>
+        </StyledFooter>
     )
 }
