528251443ac9291d715a00b7433e84cf982aef71
Phuc Tran <tqp1610@gmail.com>
2023-04-01 13:27:15 +0700

    footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,19 +1,35 @@
 import React from 'react'
-import { StyledFooter, StyledFooterInfo, StyledFooterPromo, StyledFooterPromoItem, StyledFooterMenu, StyledFooterMenuList, StyledFooterMenuItem, StyledFooterMenuTitle } from './styled';
+import { StyledFooter, StyledFooterInfo, StyledFooterContent, StyledFooterMenu, StyledFooterMenuList, StyledFooterMenuItem, StyledFooterMenuTitle, StyledFooterApply, StyledFooterInfoItem } from './styled';
+import Image from 'next/image';
+import Location from '@/assets/images/location.png';
+import Facebook from '@/assets/images/facebook.png';
+import Tiktok from '@/assets/images/tiktok.png';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 export default function Footer() {
     return (
         <StyledFooter>
-            <StyledFooterPromo>
-                <div className='row'>
-                    <div className='col-3'>
-                        <StyledFooterPromoItem>
-                        
-                        </StyledFooterPromoItem>
-                    </div>
-                </div>
-            </StyledFooterPromo>
+            <StyledFooterContent>
+                <h3 style={{ fontWeight: 600, margin: "8px 0" }}>SIGN UP FOR EMAIL</h3>
+                <p style={{ margin: "24px 0" }}>Join the BB Kids grapevine and get 20% off your first order – plus be the first to hear about news, offers and silly stuff.
+                    <br />
+                    <StyledFooterApply><a href='#'>*Exclusions apply</a></StyledFooterApply>
+                </p>
+            </StyledFooterContent>
             <StyledFooterInfo>
+                <StyledFooterInfoItem>
+                    Stores
+                    <a href='#'><Image src={Location} alt="localtion" width={30} height={30} /></a>
+                </StyledFooterInfoItem>
+                <StyledFooterInfoItem>
+                    Follow us
+                    <a href='#'><Image src={Facebook} alt="localtion" width={30} height={30} /></a>
+                    <a href='#'><Image src={Tiktok} alt="localtion" width={30} height={30} /></a>
+                </StyledFooterInfoItem>
+                <StyledFooterInfoItem>
+                    <input placeholder='Email address' />
+                    <button><ArrowForwardIcon fontSize='inherit'/></button>
+                </StyledFooterInfoItem>
 
             </StyledFooterInfo>
             <StyledFooterMenu>
