38c9d8654127db063833b2e83702e0ca22742663
Phuc Tran <tqp1610@gmail.com>
2023-04-01 13:27:37 +0700

    page

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,8 +23,9 @@ export default function Home() {
         <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,700;0,900;1,200;1,300;1,600&display=swap" rel="stylesheet" />
       </Head>
       <main>
-        Link to: <Link href="/posts/first-post"> Post</Link>
         <button onClick={logout}>Logout</button>
+
+        <button><Link href="/products/1">Detail Products</Link></button>
       </main>
     </>
   )
