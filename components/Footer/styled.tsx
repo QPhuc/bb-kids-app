9e1693aa484d4ebed397a2ed5c8717a38f5d1693
Phuc Tran <tqp1610@gmail.com>
2023-04-01 13:27:19 +0700

    footer

--- components/Footer/styled.tsx
+++ components/Footer/styled.tsx
@@ -2,24 +2,68 @@ import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
     border-top: 1px solid var(--grey-white-color);
-    margin-bottom: px;
 `
 
-export const StyledFooterPromo = styled.div`
+export const StyledFooterContent = styled.div`
     padding: 0 16px;
-    height: 40px;
-`
-
-export const StyledFooterPromoItem = styled.div`
-    
+    text-align: center;
+    padding-top: 8px;
 `
 
 export const StyledFooterInfo = styled.div`
-    background: repeating-linear-gradient(90deg,var(--primary-color), var(--primary-color-2), 5%,var(--light-color) 0,var(--light-color) 10%);
+    background: repeating-linear-gradient(90deg,var(--primary-color), var(--primary-color-2), 5%,var(--white-color) 0,var(--white-color) 10%);
     min-height: 10rem;
-    padding: 16px 0 0;
     border-top: 1px solid var(--grey-white-color);
     border-bottom: 1px solid var(--grey-white-color);
+    text-align: center;
+    text-transform: uppercase;
+    vertical-align: middle;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 100%;
+    a {
+        font-size: 40px;
+    }
+    input {
+        height: 48px;
+        width: 240px;
+        padding: 16px;
+        font-size: 14px;
+    }
+    button {
+        background-color: var(--dark-color);
+        color: var(--white-color);
+        border: none;
+        width: 48px;
+        height: 48px;
+        font-size: 32px;
+        padding: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        svg {
+            font-size: 40px;
+        }
+    }
+`
+
+export const StyledFooterInfoItem = styled.div`
+    display: flex;
+    justify-items: center;
+    align-items: center;
+    a {
+        display: flex;
+        &:first-child {
+            margin: 0 4px;
+        }
+    }
+    
+`
+
+export const StyledFooterApply = styled.p`
+    font-weight: 600;
+    text-decoration: underline;
 `
 
 export const StyledFooterMenuTitle = styled.span`
@@ -29,6 +73,10 @@ export const StyledFooterMenuTitle = styled.span`
     letter-spacing: 0.025em;
     cursor: pointer;
     margin-bottom: 16px;
+    font-weight: 500;
+    &:hover {
+        color: var(--black-color);
+    }
 `
 
 export const StyledFooterMenu = styled.div`
