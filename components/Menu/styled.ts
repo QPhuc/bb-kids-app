6855962cc19118a8c7a38c25d6dca95be54151df
Phuc Tran <tqp1610@gmail.com>
2023-04-10 20:00:27 +0700

    header

---
+++ components/Menu/styled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledMenuWrapper = styled.div`
+    width: 100%;
+    height: auto;
+    background-color: rgb(0, 0, 0, 0.3);
+    position: absolute;
+    z-index: 10000;
+    left: 0;
+    right: 0;
+
+`
